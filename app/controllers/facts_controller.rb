6198d03ec4048eb4012bbc53f8245418d91d7f7c
Arielle <arieloring@gmail.com>
2019-08-03 06:25:16 +0000

    add restrictions to update / edit / delete

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,5 +1,5 @@
 class FactsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @facts = if params[:category] == "coding"
@@ -36,16 +36,25 @@ class FactsController < ApplicationController
 
   def edit
     @fact = Fact.find(params[:id])
+    if @fact.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @fact = Fact.find(params[:id])
+    if @fact.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @fact.update_attributes(fact_params)
     redirect_to root_path
   end
 
   def destroy
     @fact = Fact.find(params[:id])
+    if @fact.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @fact.destroy
     redirect_to root_path
   end
